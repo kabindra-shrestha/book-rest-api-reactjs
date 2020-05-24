c24a4d167e880bced68617a974179873ec3dc511
Kabindra Shrestha <kabindra.shrestha.0wn@gmail.com>
2020-05-24 20:53:19 +0545

    cors added

--- src/main/java/com/mightyjava/resource/impl/BookResourceImpl.java
+++ src/main/java/com/mightyjava/resource/impl/BookResourceImpl.java
@@ -21,9 +21,8 @@ import java.util.TreeSet;
 
 @RestController
 @RequestMapping("/books")
-@CrossOrigin(origins = "http://localhost:8080")
 //@CrossOrigin(origins = "http://localhost:3000")
-//@CrossOrigin(origins = "https://book-rest-api-reactjs.herokuapp.com")
+@CrossOrigin(origins = "https://book-rest-api-reactjs.herokuapp.com")
 public class BookResourceImpl implements Resource<Book> {
 
     @Autowired
